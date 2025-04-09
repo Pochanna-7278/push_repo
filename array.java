623bb9460b5f939813dcdad51597ad99ffbb3a4b
Pochanna-7278 <pochannakotrangi@gmail.com>
2025-04-09 19:24:44 +0100

    added java file

---
+++ array.java
@@ -0,0 +1,41 @@
+import java.lang.*;
+import java.util.*;
+class arr
+{
+void largest(int a[])
+{
+int max=a[0];
+int min=a[0];
+for(int i=0;i<a.length;i++)
+{
+if(a[i]>max)
+{
+max=a[i];
+}
+if(a[i]<min)
+{
+min=a[i];
+}
+}
+System.out.println(max+"is a maximum element");
+System.out.println(min+"is a minimum element");
+}
+}
+class arraytest
+{
+public static void main (String[] args)
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter array size");
+int size=sc.nextInt();
+int a[]=new int[size];
+System.out.println("enter array elements");
+for(int i=0;i<size;i++)
+{
+a[i]=sc.nextInt();
+}
+arr obj=new arr();
+obj.largest(a);
+}
+}
+
