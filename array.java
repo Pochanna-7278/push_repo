2dfb53a483413bd7f7105a1d8c48c130d4af1a73
Pochanna-7278 <163955837+Pochanna-7278@users.noreply.github.com>
2025-04-09 18:08:35 +0100

    Update HelloWorld.java

--- array.java
+++ array.java
@@ -1,41 +1,5 @@
-import java.lang.*;
-import java.util.*;
-class arr
-{
-void largest(int a[])
-{
-int max=a[0];
-int min=a[0];
-for(int i=0;i<a.length;i++)
-{
-if(a[i]>max)
-{
-max=a[i];
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
-if(a[i]<min)
-{
-min=a[i];
-}
-}
-System.out.println(max+"is a maximum element");
-System.out.println(min+"is a minimum element");
-}
-}
-class array
-{
-public static void main (String[] args)
-{
-Scanner sc=new Scanner(System.in);
-System.out.println("enter array size");
-int size=sc.nextInt();
-int a[]=new int[size];
-System.out.println("enter array elements");
-for(int i=0;i<size;i++)
-{
-a[i]=sc.nextInt();
-}
-arr obj=new arr();
-obj.largest(a);
-}
-}
-
